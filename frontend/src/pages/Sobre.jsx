7b3e88c0e0aaa9940eb57bb4125dbc0319bac698
Campusero123 <augustocesar.ffalcao@gmail.com>
2021-03-11 14:55:53 -0300

    Modificações na Estilização da Page Sobre
    - Alinhamento correto com informações e título à direita da imagem do integrante.
    
    - Alteração no tamanho da fonte

--- frontend/src/pages/Sobre.jsx
+++ frontend/src/pages/Sobre.jsx
@@ -20,37 +20,33 @@ const Sobre = () => (
 
         {' '}
         <div className="text-center texto-card">
-          <Image className="Imagem Augusto" src={ImageAugusto} roundedCircle />
-          <br />
-          Augusto Falcão
-          <br />
-          <br />
-          Fala galera, sou o Augusto. Sou geek, louco por tecnologia e arte. Sou cinéfilo, gosto muito de esportes, música e design.
-          <br />
-          <br />
-          Escolhi a Engenharia de Software pois sou criativo, e sempre foi meu sonho usar minha força de trabalho para criar e ajudar a solucionar as dores das pessoas, e sempre quis ser um engenheiro, um líder e empreendedor.
+          <Image className="ImagemAugusto" src={ImageAugusto} roundedCircle />
+          <div className="Container1">
+            Augusto Falcão
+            <br />
+            <h3>Fala galera, sou o Augusto. Sou geek, louco por tecnologia e arte. Sou cinéfilo, gosto muito de esportes, música e design.</h3>
+            <h3>Escolhi a Engenharia de Software pois sou criativo, e sempre foi meu sonho usar minha força de trabalho para criar e ajudar a solucionar as dores das pessoas, e sempre quis ser um engenheiro, um líder e empreendedor.</h3>
+          </div>
           <br />
           <br />
-          <Image className="Imagem Diogo" src={ImageDiogo} roundedCircle />
-          <br />
-          Diogo
-          <br />
-          <br />
-          Olá!! Me chamo Diogo e sou apaixonado por tecnologia e pelo mundo nerd, desde pequeno sempre nas telas seja no Super Nintendo, Cinema ou no Computador. Sempre curioso por tudo.
-          <br />
-          <br />
-          Então resolvi juntar minhas duas paixões numa página no Instagram chamada Cômodo Nerd!
+          <Image className="ImagemDiogo" src={ImageDiogo} roundedCircle />
+          <div className="Container2">
+            Diogo
+            <br />
+            <h3>Olá!! Me chamo Diogo e sou apaixonado por tecnologia e pelo mundo nerd, desde pequeno sempre nas telas seja no Super Nintendo, Cinema ou no Computador. Sempre curioso por tudo.</h3>
+            <h3>Então resolvi juntar minhas duas paixões numa página no Instagram chamada Cômodo Nerd!</h3>
+          </div>
           <br />
           <br />
-          <Image className="rounded mx-auto d-block mb-4" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
+          <Image className="ImgGenerica" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
           Thiago
           <br />
           <br />
-          <Image className="rounded mx-auto d-block mb-4" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
+          <Image className="ImgGenerica" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
           Tiago
           <br />
           <br />
-          <Image className="rounded mx-auto d-block mb-4" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
+          <Image className="ImgGenerica" src="https://cdn0.iconfinder.com/data/icons/bitcoin-cryptocurrency-lavender-vol-2-1/512/cypherpunk-512.png" roundedCircle />
           Alan
           <br />
           <br />
